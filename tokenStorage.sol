3ac49a55e58c0075ecd390ad98e68f23ca9d6e48
Tirthraj Chavan <tirthrajchavan07@gmail.com>
2026-04-19 15:24:42 +0530

    tokenStorage.sol
    This project is a simple Solidity smart contract that stores a number on the blockchain. You can set the number using a function and retrieve it anytime. It is useful for beginners to understand basic smart contract concepts.

--- tokenStorage.sol
+++ tokenStorage.sol
@@ -1,1 +1,21 @@
+// SPDX-License-Identifier: MIT
+//Version used (0.8.0)
+pragma solidity ^0.8.0;
 
+//Contract name 
+contract Storage {
+
+    // State variable to store a number
+    uint public storedNumber;
+
+    // Function to set the number
+    function setNumber(uint _number) public {
+        // Assign the value 
+        storedNumber = _number;
+    }
+
+    // Function to get the stored number
+    function getNumber() public view returns (uint) {
+        return storedNumber;
+    }
+}
