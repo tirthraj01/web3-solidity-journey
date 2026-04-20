86b467db7965872d810c35d3d3093920c03b5ac5
Tirthraj Chavan <tirthrajchavan07@gmail.com>
2026-04-20 23:06:37 +0530

    balance_tracker2.sol
    This project is a simple Solidity smart contract that tracks user balances using a mapping.
    Users can deposit and withdraw amounts, and also check their current balance.
    It helps beginners understand how balance management works in smart contracts.

--- balance_tracker2.sol
+++ balance_tracker2.sol
@@ -1,1 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+contract balanceTracker{
+
+    mapping(address => uint256) public balance;
+    function deposit(uint256 amount) public  {
+        balance[msg.sender] += amount;
+
+    }
+    function withdraw(uint amount) public {
+        require(balance [msg.sender] >= amount , "Insufficient Balance" );
+        balance[msg.sender] -= amount;
+    }
+
+    function CurrentBalance(address user) public view returns (uint256) {
+        return balance[user];
+    }
+}
